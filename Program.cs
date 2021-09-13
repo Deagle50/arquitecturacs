71f6fdcb19ea024d7e2b1a48a0d95704996f08a6
srlopez <you@example.com>
2021-09-13 15:31:43 +0200

    paso5

--- Program.cs
+++ Program.cs
@@ -24,11 +24,13 @@ Console.WriteLine("Empezamos");
 //var notas = new[] { 7.5M, 4, 6, 5, 4, 6.5M, 7.5M };
 
 // Tuplas
+// Tuplas
 var tLuis = (Nombre: "Luis", Nota: 7.5M);
 var tMarta = (Nombre: "Marta", Nota: 4M);
-var tNotas = new[] { tLuis, tMarta };
+var tMarcos = ("Marcos", 6M);  //<--- Ojo! sin nombre
+var tNotas = new[] { tLuis, tMarta, tMarcos };
 Tuple<string, int>[] tNotas0 = new Tuple<string, int>[] { }; //<-- inicializado a 0 elementos
-Console.WriteLine($"La primera nota del array de tuplas es {tNotas[0].Nota:0.00}");
+Console.WriteLine($"La primera nota del array de tuplas es {tNotas[0].Item1:0.00}");
 
 // Estructuras
 var sLuis = new SCalificacion("Luis", 7.5M);
